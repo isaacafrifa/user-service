f32b4ea161f4d2d4b622a4b3560abe6d3951c084
Afrifa <isaac.afrifa3@yahoo.com>
2024-10-02 00:27:52 +0300

    feat: add CORS configuration for API Gateway Swagger UI access

---
+++ src/main/java/iam/userservice/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package iam.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for CORS (Cross-Origin Resource Sharing) settings.
+ * It's needed for the API Gateway's swagger UI to be able to access this API's Swagger UI.
+ * This class implements the WebMvcConfigurer interface to customize the CORS configuration.
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+}
