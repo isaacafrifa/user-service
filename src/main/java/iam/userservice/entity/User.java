5eb8ca880f8a3b91def4babd447e544e2356d570
Afrifa <isaac.afrifa3@yahoo.com>
2024-11-16 01:43:13 +0200

    fix: make MapStructs to recognize the Mapping sources in UserMapper

--- src/main/java/iam/userservice/entity/User.java
+++ src/main/java/iam/userservice/entity/User.java
@@ -8,9 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,8 +16,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.OffsetDateTime;
 
 @Entity
-@Getter
-@Setter
 @Table(name = "users")
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
@@ -49,4 +45,58 @@ public class User {
     @Version
     private Long version;
 
+    /*
+     * Explicitly define getter and setter methods to ensure MapStruct can properly
+     * detect and use them for the User class, as it fails to recognize
+     * the default Lombok-generated getters.
+     */
+    public Long getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public OffsetDateTime getCreatedOn() {
+        return createdOn;
+    }
+    public OffsetDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setCreatedOn(OffsetDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+    public void setUpdatedOn(OffsetDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+    public void setVersion(Long version) {
+        this.version = version;
+    }
 }
